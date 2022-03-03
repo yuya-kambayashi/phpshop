3886d543f8d5562d351113f208f0cc6ee05c6402
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-03 17:51:14 +0900

    add staff_add_done

---
+++ staff/staff_add_done.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+      <?php
+
+    try{
+
+      $staff_name=$_POST['name'];
+      $staff_pass=$_POST['pass'];
+
+      $staff_name=htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
+      $staff_pass=htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
+
+      $dns = 'mysql:dbname=shop;host=localhost;sharset=utf8';
+      $user = 'root';
+      $password = '';
+      $dbh = new PDO($dns, $user, $password);
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      $sql = 'INSERT INTO mst_staff(name, password) VALUES (?,?)';
+      $stmt = $dbh->prepare($sql);
+      $data[] = $staff_name;
+      $data[] = $staff_pass;
+      $stmt->execute($data);
+
+      $dbh = null;
+
+      print $staff_name;
+      print 'さんを追加しました。<br />';
+
+    }catch (Exception $e){
+      print 'ただいま障害により大変ご迷惑をおかけしております。';
+      exit();
+    }
+
+    ?>
+
+    <a href="staff_list.php">戻る</a>
+
+  </body>
+</html>
