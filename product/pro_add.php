e1202d1f1ebd9deff3459684e660c304cfc74ea0
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-06 01:25:06 +0900

    product用のフォルダを追加します

---
+++ product/pro_add.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+    商品追加<br />
+    <br />
+    <form method="post" action="pro_add_check.php">
+      商品名を入力してください。<br />
+      <input type="text" name="name" style="width:200px"><br />
+      価格を入力してください。<br />
+      <input type="text" name="pass" style="width:100px"><br />
+      <br />
+      <input type="button" onclick="history.back()" value="戻る">
+      <input type="submit" value="OK">
+</form>
+  </body>
+</html>
