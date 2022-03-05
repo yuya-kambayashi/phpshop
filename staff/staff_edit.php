12e7f03b459108921f24a421c614d001033c178a
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-05 23:55:21 +0900

    add staff_edit

---
+++ staff/staff_edit.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+      <?php
+
+    try{
+
+      $staff_code = $_POST['staffcode'];
+
+      $dns = 'mysql:dbname=shop;host=localhost;sharset=utf8';
+      $user = 'root';
+      $password = '';
+      $dbh = new PDO($dns, $user, $password);
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      $sql = 'SELECT name FROM mst_staff where code=?';
+      $stmt = $dbh->prepare($sql);
+      $data[]=$staff_code;
+      $stmt->execute($data);
+
+      $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+      $staff_name=$rec['name'];
+
+      $dbh = null;
+
+    }catch (Exception $e){
+      print 'ただいま障害により大変ご迷惑をおかけしております。';
+      exit();
+    }
+
+    ?>
+
+    スタッフ修正<br />
+    <br />
+    スタッフコード<br />
+    <?php print $staff_code; ?>
+    <br />
+    <br />
+    <form method="post"action="staff_edit_check.php">
+    <input type="hidden" name="code" value="<?php print $staff_code; ?>">
+    スタッフ名<br />
+    <input type="text" name="name" style="width:200px" value="<?php print $staff_name;?>"><br />
+    パスワードを入力してください。<br />
+    <input type="password" name="pass" style="width:100p"><br />
+    パスワードをもう１度入力してください。<br />
+    <input type="password" name="pass2" style="width:100p"><br />
+    <br />
+    <input type="button" onclick="history.back()" value="戻る">
+    <input type="submit" value="OK">
+  </fomr>
+
+  </body>
+</html>
