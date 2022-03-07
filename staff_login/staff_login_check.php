f742513827c4d6b4e6e357195b3bf68fd2e517c5
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-08 01:19:20 +0900

    add staff_login_check

---
+++ staff_login/staff_login_check.php
@@ -0,0 +1,42 @@
+<?php
+
+    try{
+            
+        $staff_code=$_POST['code'];
+        $staff_pass=$_POST['pass'];
+
+        $staff_code=htmlspecialchars($staff_code,ENT_QUOTES,'UTF-8');
+        $staff_pass=htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
+
+        $staff_pass=md5($staff_pass);
+
+        $dns = 'mysql:dbname=shop;host=localhost;charset=utf8';
+        $user = 'root';
+        $password = '';
+        $dbh = new PDO($dns, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = 'SELECT name FROM mst_staff where code = ? AND password = ?';
+        $stmt = $dbh->prepare($sql);
+        $data[]=$staff_code;
+        $data[]=$staff_pass;
+        $stmt->execute($data);
+
+        $dbh = null;
+
+        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($rec==false){
+            print 'スタッフコードかパスワードが間違っています。<br />';
+            print '<a href="staff_login.html">戻る</a>';
+        }
+        else{
+            header('Location: staff_top.php');
+            exit();   
+        }
+    }catch (Exception $e){
+        print 'ただいま障害により大変ご迷惑をおかけしております。';
+        exit();
+    }
+
+?>
