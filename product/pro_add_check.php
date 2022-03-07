1f247cdaaf9d32af2c41363c94485d65d825405b
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-08 00:18:28 +0900

    edit pro_add

--- product/pro_add_check.php
+++ product/pro_add_check.php
@@ -9,6 +9,7 @@
 
       $pro_name=$_POST['name'];
       $pro_price=$_POST['price'];
+      $pro_gazou=$_FILES['gazou'];
 
       $pro_name=htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');
       $pro_price=htmlspecialchars($pro_price,ENT_QUOTES,'UTF-8');
@@ -31,7 +32,18 @@
         print '<br />';
       }
 
-      if($pro_name=='' || preg_match('/\A[0-9]+\z/', $pro_price)==0){
+      if ($pro_gazou['size'] > 0){
+        if($pro_gazou['size'] > 10000000){
+          print "画像が大きすぎます";
+        }
+        else{
+          move_uploaded_file($pro_gazou['tmp_name'],'./gazou/'.$pro_gazou['name']);
+          print '<img src = "./gazou/'.$pro_gazou['name'].'">';
+          print '<br />';
+        }
+      }
+
+      if($pro_name=='' || preg_match('/\A[0-9]+\z/', $pro_price)==0 || $pro_gazou['size'] > 10000000){
           print '<form>';
           print '<input type="button" onclick="history.back()" value="戻る">';
           print '</form>';
@@ -41,6 +53,7 @@
           print '<form method="post" action="pro_add_done.php">';
           print '<input type="hidden" name="name" value="'.$pro_name.'">';
           print '<input type="hidden" name="price" value="'.$pro_price.'">';
+          print '<input type="hidden" name="gazou" value="'.$pro_gazou['name'].'">';
           print '<br />';
           print '<input type="button" onclick="history.back()" value="戻る">';
           print '<input type="submit" value="ＯＫ">';
