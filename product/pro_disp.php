47ccc210d9847bf7ba9e45f34a2e5b8d2681c1f6
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-07 14:01:42 +0900

    add pro_disp

---
+++ product/pro_disp.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+      <?php
+
+    try{
+
+      $pro_code = $_GET['procode'];
+
+      $dns = 'mysql:dbname=shop;host=localhost;charset=utf8';
+      $user = 'root';
+      $password = '';
+      $dbh = new PDO($dns, $user, $password);
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      $sql = 'SELECT name, price FROM mst_product where code=?';
+      $stmt = $dbh->prepare($sql);
+      $data[]=$pro_code;
+      $stmt->execute($data);
+
+      $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+      $pro_name=$rec['name'];
+      $pro_price=$rec['price'];
+
+      $dbh = null;
+
+    }catch (Exception $e){
+      print 'ただいま障害により大変ご迷惑をおかけしております。';
+      exit();
+    }
+
+    ?>
+
+    商品情報参照<br />
+    <br />
+    商品コード<br />
+    <?php print $pro_code; ?>
+    <br />
+    商品名<br />
+    <?php print $pro_name; ?>
+    <br />
+    価格<br />
+    <?php print $pro_price; ?>円
+    <br />
+    <br />
+    <input type="button" onclick="history.back()" value="戻る">
+  </fomr>
+
+  </body>
+</html>
