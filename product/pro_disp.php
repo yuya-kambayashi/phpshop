e869fc10fb8277eb44ba37afe1f9c116105aef06
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-08 00:25:44 +0900

    show image at pro_list

--- product/pro_disp.php
+++ product/pro_disp.php
@@ -17,7 +17,7 @@
       $dbh = new PDO($dns, $user, $password);
       $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-      $sql = 'SELECT name, price FROM mst_product where code=?';
+      $sql = 'SELECT name, price, gazou FROM mst_product where code=?';
       $stmt = $dbh->prepare($sql);
       $data[]=$pro_code;
       $stmt->execute($data);
@@ -25,8 +25,17 @@
       $rec = $stmt->fetch(PDO::FETCH_ASSOC);
       $pro_name=$rec['name'];
       $pro_price=$rec['price'];
+      $pro_gazou_name=$rec['gazou'];
 
       $dbh = null;
+
+      if ($pro_gazou_name==''){
+          $disp_gazou='';
+      }
+      else{
+        $disp_gazou = '<img src = "./gazou/'.$pro_gazou_name.'">';
+
+      }
 
     }catch (Exception $e){
       print 'ただいま障害により大変ご迷惑をおかけしております。';
@@ -45,6 +54,8 @@
     <br />
     価格<br />
     <?php print $pro_price; ?>円
+    <br />
+    <?php print $disp_gazou; ?>
     <br />
     <br />
     <input type="button" onclick="history.back()" value="戻る">
