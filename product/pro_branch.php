05189a00234ef436757eb5086846178eb8ef4492
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-07 13:59:03 +0900

    add pro_ng

--- product/pro_branch.php
+++ product/pro_branch.php
@@ -1,13 +1,21 @@
 <?php
 
 if(isset($_POST['edit']) == true ){
+    if(isset($_POST['procode']) == false ){
 
+        header('Location: pro_ng.php');
+        exit();
+    }
     $pro_code=$_POST['procode'];
     header('Location: pro_edit.php?procode='.$pro_code);
     exit();
 }
 if(isset($_POST['delete']) == true ){
+    if(isset($_POST['procode']) == false ){
 
+        header('Location: pro_ng.php');
+        exit();
+    }
     $pro_code=$_POST['procode'];
     header('Location: pro_delete.php?procode='.$pro_code);
     exit();
---
+++ product/pro_ng.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+
+  商品が選択されていません。<br />
+  <a href="pro_list.php">戻る</a>
+
+  </body>
+</html>
