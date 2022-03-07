f12c8caff72194a8246f7f0e829efe8f7b27c4b8
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-07 12:13:00 +0900

    add pro_add_done

---
+++ product/pro_add_done.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+      <?php
+
+    try{
+
+      $pro_name=$_POST['name'];
+      $pro_price=$_POST['price'];
+
+      $pro_name=htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');
+      $pro_price=htmlspecialchars($pro_price,ENT_QUOTES,'UTF-8');
+
+      $dns = 'mysql:dbname=shop;host=localhost;charset=utf8';
+      $user = 'root';
+      $password = '';
+      $dbh = new PDO($dns, $user, $password);
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      $sql = 'INSERT INTO mst_product(name, price) VALUES (?,?)';
+      $stmt = $dbh->prepare($sql);
+      $data[] = $pro_name;
+      $data[] = $pro_price;
+      $stmt->execute($data);
+
+      $dbh = null;
+
+      print $pro_name;
+      print 'を追加しました。<br />';
+
+    }catch (Exception $e){
+      print 'ただいま障害により大変ご迷惑をおかけしております。';
+      exit();
+    }
+
+    ?>
+
+    <a href="pro_list.php">戻る</a>
+
+  </body>
+</html>
