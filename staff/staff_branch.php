ba6814a1bbd7acaa4376505072e91aabb4b0c2b5
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-07 13:57:20 +0900

    add staff_ng

--- staff/staff_branch.php
+++ staff/staff_branch.php
@@ -2,12 +2,22 @@
 
 if(isset($_POST['edit']) == true ){
 
+    if(isset($_POST['staffcode']) == false ){
+
+        header('Location: staff_ng.php');
+        exit();
+    }
     $staff_code=$_POST['staffcode'];
     header('Location: staff_edit.php?staffcode='.$staff_code);
     exit();
 }
 if(isset($_POST['delete']) == true ){
+    
+    if(isset($_POST['staffcode']) == false ){
 
+        header('Location: staff_ng.php');
+        exit();
+    }
     $staff_code=$_POST['staffcode'];
     header('Location: staff_delete.php?staffcode='.$staff_code);
     exit();
---
+++ staff/staff_ng.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+
+  スタッフが選択されていません。<br />
+  <a href="staff_list.php">戻る</a>
+
+  </body>
+</html>
