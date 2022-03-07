3ae28b038adc4e5d28a71df4d861234a5f7fa153
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-07 17:06:21 +0900

    add pro_edit_done

---
+++ product/pro_edit_done.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+      <?php
+
+    try{
+
+      $pro_code=$_POST['code'];
+      $pro_name=$_POST['name'];
+      $pro_price=$_POST['price'];
+
+      $pro_code=htmlspecialchars($pro_code,ENT_QUOTES,'UTF-8');
+      $pro_name=htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');
+      $pro_price=htmlspecialchars($pro_price,ENT_QUOTES,'UTF-8');
+
+      $dns = 'mysql:dbname=shop;host=localhost;charset=utf8';
+      $user = 'root';
+      $password = '';
+      $dbh = new PDO($dns, $user, $password);
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      $sql = 'UPDATE mst_product SET name=?, price=? WHERE code=?';
+      $stmt = $dbh->prepare($sql);
+      $data[] = $pro_name;
+      $data[] = $pro_price;
+      $data[] = $pro_code;
+      $stmt->execute($data);
+
+      $dbh = null;
+
+      print '修正しました。<br />';
+
+    }catch (Exception $e){
+      print 'ただいま障害により大変ご迷惑をおかけしております。';
+      exit();
+    }
+
+    ?>
+
+    <a href="pro_list.php">戻る</a>
+
+  </body>
+</html>
