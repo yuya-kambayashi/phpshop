238d3870bf06359e2384e41eeb3d5809574e66f9
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-09 17:52:20 +0900

    add staff_top

---
+++ staff_login/staff_top.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+
+  ショップ管理トップメニュー<br />
+  <br />
+  <a href="../staff/staff_list.php">スタッフ管理</a><br />
+  <br />
+  <a href="../product/pro_list.php">商品管理</a><br />
+    </body>
+</html>
