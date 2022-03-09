b0488730fdd054cd4cb0e55dce3f97d4a9d43b42
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-09 18:17:51 +0900

    mod staff_top

--- staff_login/staff_top.php
+++ staff_login/staff_top.php
@@ -6,6 +6,11 @@ if(isset($_SESSION['login'])==false){
   print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
   exit();
 }
+else{
+  print $_SESSION['staff_name'];
+  print 'さんログイン中<br />';
+  print '<br />';
+}
 
 ?>
 
