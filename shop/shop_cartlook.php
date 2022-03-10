0ac9011c41ca734ef32ecc25c52ce5321b230314
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-11 00:28:10 +0900

    edit shop_cartlook

--- shop/shop_cartlook.php
+++ shop/shop_cartlook.php
@@ -30,6 +30,13 @@
       $kazu = $_SESSION['kazu'];
       $max = count($cart);
 
+      if ($max == 0){
+        print "カートに商品が入っていません。<br />";
+        print "<br />";
+        print '<a href = "shop_list.php">商品一覧に戻る</a>';
+        exit();
+      }
+
       $dns = 'mysql:dbname=shop;host=localhost;charset=utf8';
       $user = 'root';
       $password = '';
