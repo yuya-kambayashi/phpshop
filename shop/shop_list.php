0ca26b417dbb9f3aa0769fcdbd26af651993f3a8
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-10 10:40:00 +0900

    add shop_list

---
+++ shop/shop_list.php
@@ -0,0 +1,63 @@
+<?php
+  session_start();
+  session_regenerate_id(true);
+  if(isset($_SESSION['member_login'])==false){
+    print 'ようこそゲスト様　';
+    print '<a href="member_login.html">会員ログイン</a><br />';
+    print '<br />';
+  }
+  else{
+    print 'ようこそ<br />';
+    print $_SESSION['member_name'];
+    print '様　';
+    print '<a href="member_logout.php">ログアウト</a><br />';
+    print '<br />';
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+      <?php
+
+    try{
+      $dns = 'mysql:dbname=shop;host=localhost;charset=utf8';
+      $user = 'root';
+      $password = '';
+      $dbh = new PDO($dns, $user, $password);
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      $sql = 'SELECT code,name, price FROM mst_product where 1';
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute();
+
+      $dbh = null;
+
+      print '商品一覧<br /><br />';
+
+      while(true){
+
+        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($rec==false){
+          break;
+        }
+
+        print '<a href="shop_product.php?procode='.$rec['code'].'">';
+        print $rec['name'].'---';
+        print $rec['price'].'円';
+        print '<a />';
+        print '<br />';
+      }
+
+    }catch (Exception $e){
+      print 'ただいま障害により大変ご迷惑をおかけしております。';
+      exit();
+    }
+
+    ?>
+  </body>
+</html>
