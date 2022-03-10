e0b38ee1520c187beee4d5b849842190e55b06f9
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-10 18:18:37 +0900

    add clear_cart

---
+++ shop/clear_cart.php
@@ -0,0 +1,22 @@
+<?php
+  session_start();
+  $_SESSION=array();
+  if(isset($_COOKIE[session_name()])==true){
+    
+    setcookie(session_name(), '', time()-42000,'/');
+  }
+  session_destroy();
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+
+  カートを空にしました。<br />
+
+  </body>
+</html>
