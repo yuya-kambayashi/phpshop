87b5fb6c68530a65b25cacb7356f24fbcd891100
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-11 20:13:51 +0900

    mod shop_cartlook

--- shop/shop_cartlook.php
+++ shop/shop_cartlook.php
@@ -78,18 +78,40 @@
     カートの中身<br />
     <br />
     <form method="post" action="kazu_change.php">
+      <table border="1">
+        <tr>
+          <td>商品</td>
+          <td>商品画像</td>
+          <td>価格</td>
+          <td>数量</td>
+          <td>小計</td>
+          <td>削除</td>
+        </tr> 
       <?php for($i=0; $i < $max; $i++){
       ?>
-
-        <?php print $pro_name[$i]; ?>
-        <?php print $pro_gazou[$i]; ?>
-        <?php print $pro_price[$i];?>円
-        <input type="text" name="kazu<?php print $i;?>" value="<?php print $kazu[$i];?>">
-        <?php print $pro_price[$i] * $kazu[$i];?>円
-        <input type="checkbox" name="sakujo<?php print $i;?>">
-        <br />
+        <tr>
+          <td>
+            <?php print $pro_name[$i]; ?>
+          </td>
+          <td>
+            <?php print $pro_gazou[$i]; ?>
+          </td>
+          <td>
+            <?php print $pro_price[$i];?>円
+          </td>
+          <td>
+            <input type="text" name="kazu<?php print $i;?>" value="<?php print $kazu[$i];?>">
+          </td>
+          <td>
+            <?php print $pro_price[$i] * $kazu[$i];?>円
+          </td>
+          <td>
+            <input type="checkbox" name="sakujo<?php print $i;?>">
+          </td>
+        </tr>
       <?php } 
       ?>
+      </table>
       <input type="hidden" name="max" value="<?php print $max;?>">
       <input type="submit" value="数量変更"><br />
       <input type="button" onclick="history.back()" value="戻る">
