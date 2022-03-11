aa2b47e4d71e840a21df200baa266b558f7edd6f
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-11 19:49:59 +0900

    mod kazu_change

--- shop/kazu_change.php
+++ shop/kazu_change.php
@@ -9,8 +9,14 @@
   $max = $_POST['max'];
   for($i = 0; $i < $max; $i++){
 
-    if(preg_match("/\A[0-9]+\z/", $post['kazu'.$i])==0){
+    if (preg_match("/\A[0-9]+\z/", $post['kazu'.$i])==0){
       print '数量に誤りがあります。<br />';
+      print '<a href="shop_cartlook.php">カートに戻る</a>';
+      exit();
+    }
+
+    if ($post['kazu'.$i] < 1 || 10 < $post['kazu'.$i]){
+      print '数量は必ず1以上10個までです。<br />';
       print '<a href="shop_cartlook.php">カートに戻る</a>';
       exit();
     }
