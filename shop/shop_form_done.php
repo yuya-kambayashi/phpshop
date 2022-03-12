e7563730cc2737cdd7f3b091d10db2a847ffaf3a
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-13 01:03:32 +0900

    mod shop_form_done

--- shop/shop_form_done.php
+++ shop/shop_form_done.php
@@ -1,3 +1,8 @@
+<?php
+  session_start();
+  session_regenerate_id(true);
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -7,24 +12,30 @@
   <body>
     <?php
 
-    require_once('../common/common.php');
+      try{
+        require_once('../common/common.php');
 
-    $post = sanitize($_POST);
+        $post = sanitize($_POST);
 
-    $onamae = $post['onamae'];
-    $email = $post['email'];
-    $postal1 = $post['postal1'];
-    $postal2 = $post['postal2'];
-    $address = $post['address'];
-    $tel = $post['tel'];
+        $onamae = $post['onamae'];
+        $email = $post['email'];
+        $postal1 = $post['postal1'];
+        $postal2 = $post['postal2'];
+        $address = $post['address'];
+        $tel = $post['tel'];
 
-    print $onamae.'様<br />';
-    print 'ご注文ありがとうございました。<br />';
-    print $email.'にメールをお送りしましたのでご確認ください。<br />';
-    print '商品は以下の住所に発送させていただきます。<br />';
-    print $postal1.'-'.$postal2.'<br />';
-    print $address.'<br />';
-    print $tel.'<br />';
+        print $onamae.'様<br />';
+        print 'ご注文ありがとうございました。<br />';
+        print $email.'にメールをお送りしましたのでご確認ください。<br />';
+        print '商品は以下の住所に発送させていただきます。<br />';
+        print $postal1.'-'.$postal2.'<br />';
+        print $address.'<br />';
+        print $tel.'<br />';
+        
+      }catch (Exception $e){
+        print 'ただいま障害により大変ご迷惑をおかけしております。';
+        exit();
+      }
 
     ?>
   </body>
