da816d0c997feaabe22d470fdc1804906ff2dd6c
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-13 01:01:19 +0900

    add shop_form_done

---
+++ shop/shop_form_done.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+    <?php
+
+    require_once('../common/common.php');
+
+    $post = sanitize($_POST);
+
+    $onamae = $post['onamae'];
+    $email = $post['email'];
+    $postal1 = $post['postal1'];
+    $postal2 = $post['postal2'];
+    $address = $post['address'];
+    $tel = $post['tel'];
+
+    print $onamae.'様<br />';
+    print 'ご注文ありがとうございました。<br />';
+    print $email.'にメールをお送りしましたのでご確認ください。<br />';
+    print '商品は以下の住所に発送させていただきます。<br />';
+    print $postal1.'-'.$postal2.'<br />';
+    print $address.'<br />';
+    print $tel.'<br />';
+
+    ?>
+  </body>
+</html>
