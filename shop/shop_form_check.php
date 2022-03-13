c09b8829ed8779c0f4bb42c09444dc5ddfe71c17
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-14 01:15:48 +0900

    mod shop_form_check

--- shop/shop_form_check.php
+++ shop/shop_form_check.php
@@ -17,6 +17,11 @@
     $postal2 = $post['postal2'];
     $address = $post['address'];
     $tel = $post['tel'];
+    $chumon = $post['chumon'];
+    $pass = $post['pass'];
+    $pass2 = $post['pass2'];
+    $danjo = $post['danjo'];
+    $birth = $post['birth'];
 
     $okflg = true;
 
@@ -75,6 +80,47 @@
       print '<br /><br />';
     }
 
+    if($chumon == 'chumontouroku'){
+      if($pass==""){
+        print 'パスワードが入力されていません。<br /><br />';
+        $okflg = false;
+      }
+
+      if($pass2==""){
+        print '確認用パスワードが入力されていません。<br /><br />';
+        $okflg = false;
+      }
+
+      if($pass==$pass2){
+        print 'パスワードが一致しません。<br /><br />';
+        $okflg = false;
+      }
+
+      print '性別<br />';
+      if($danjo=='dan'){
+        print '男性';
+      }
+      elseif ($danjo=='jo'){
+        print '女性';
+      }
+      elseif ($danjo=='sonota'){
+        print 'その他';
+      }
+      elseif ($danjo=='noanswer'){
+        print '答えない';
+      }
+
+      print '<br /><br />';
+
+      print '生まれ年<br />';
+      print $birth.'年代';
+
+      print '<br /><br />';
+
+
+    }
+
+
     if ($okflg==true){
 
       print '<form method="post" action="shop_form_done.php">';
@@ -84,6 +130,10 @@
       print '<input type="hidden" name="postal2" value="'.$postal2.'">';
       print '<input type="hidden" name="address" value="'.$address.'">';
       print '<input type="hidden" name="tel" value="'.$tel.'">';
+      print '<input type="hidden" name="chumon" value="'.$chumon.'">';
+      print '<input type="hidden" name="pass" value="'.$pass.'">';
+      print '<input type="hidden" name="danjo" value="'.$danjo.'">';
+      print '<input type="hidden" name="birth" value="'.$birth.'">';
       print '<input type="button" onclick="history.back()" value="戻る" />';
       print '<input type="submit" value="OK" /><br />';
       print '</form>';
@@ -93,6 +143,7 @@
       print '<input type="button" onclick="history.back()" value="戻る" />';
       print '</form>';
     }
+
 
 
     ?>
