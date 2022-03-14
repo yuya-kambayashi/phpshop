26b27b69f28799be595ef5cd03adf1d7da5f7575
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-15 00:37:28 +0900

    add db lock

--- shop/shop_form_done.php
+++ shop/shop_form_done.php
@@ -107,6 +107,10 @@
 
         }
 
+        $sql = 'LOCK TABLES dat_sales WRITE, dat_sales_product WRITE';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute($data);
+
         $sql = 'INSERT INTO dat_sales( code_member, name, email, postal1, postal2, address, tel) VALUES (?,?,?,?,?,?,?) ';
         $stmt = $dbh->prepare($sql);
         $data = array();
@@ -138,6 +142,9 @@
 
         }
 
+        $sql = 'UNLOCK TABLES';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute($data);
 
         $dbh = null;
 
