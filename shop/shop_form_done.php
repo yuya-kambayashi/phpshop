ea106cfdca3364cc9f996cdfe7d93b05eabbbc52
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-15 00:13:56 +0900

    mod shop_form_done

--- shop/shop_form_done.php
+++ shop/shop_form_done.php
@@ -23,6 +23,11 @@
         $postal2 = $post['postal2'];
         $address = $post['address'];
         $tel = $post['tel'];
+        $chumon = $post['chumon'];
+        $pass = $post['pass'];
+        $danjo = $post['danjo'];
+        $birth = $post['birth'];
+        
 
         print $onamae.'様<br />';
         print 'ご注文ありがとうございました。<br />';
@@ -55,9 +60,41 @@
           $data[0]=$cart[$i];
           $stmt->execute($data);
 
+          // $lastmembercode=0;
+
+          // if($chumon=='chumontouroku'){
+
+          //   $sql = 'INSERT INTO dat_member(password, name, email, postal1, postal2, address, tel, danjo, born) VALUES(?,?,?,?,?,?,?,?,?)';
+          //   $stmt= $dbh->prepare($sql);
+          //   $data[] = array();
+          //   $data[] = md5($pass);
+          //   $data[] = $onamae;
+          //   $data[] = $email;
+          //   $data[] = $postal1;
+          //   $data[] = $postal2;
+          //   $data[] = $address;
+          //   $data[] = $tel;
+          //   if ( $danjo == 'dan'){
+          //     $data[] = 1;
+          //   }
+          //   elseif ($danjo=='jo'){
+          //     $data[] = 2;
+          //   }
+          //   elseif ($danjo=='sonota'){
+          //     $data[] = 3;
+          //   }
+          //   elseif ($danjo=='noanswer'){
+          //     $data[] = 4;
+          //   }
+          //   $data[] = $birth;
+          //   $stmt->execute($data);
+          //   $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+          //   $lastmembercode = $rec['LAST_INSERT_ID()'];
+          // }
+
           $rec = $stmt->fetch(PDO::FETCH_ASSOC);
 
-          $name = $rec['name'];
+          $name = $rec  name'];
           $price = $rec['price'];
           $suryo = $kazu[$i];
           $shokei = $price * $suryo;
