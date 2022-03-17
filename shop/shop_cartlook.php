24fd32db51b8b12881cb45e9e038c66645dbe0be
yuya-kambayashi <kambayashi73@gmail.com>
2022-03-17 23:46:47 +0900

    add link to kantan

--- shop/shop_cartlook.php
+++ shop/shop_cartlook.php
@@ -118,5 +118,11 @@
     </form>
     <a href="shop_form.html">ご購入手続きへ進む</a><br />
 
+    <?php
+      if(isset($_SESSION["member_login"]) == true){
+        print '<a href="shop_kantan_check.html">会員かんたん注文へ進む</a><br />';
+      }
+    ?>
+
   </body>
 </html>
